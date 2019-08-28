4b623d548b3bef49ba7b49b66e5dbaa0539cec96
Oriol Alfonso Riba <oriolalfonso91@gmail.com>
2019-08-28 19:20:39 +0200

    Change middleware mock and test it using normal view calls

--- crossbox/tests/test_auth.py
+++ crossbox/tests/test_auth.py
@@ -3,30 +3,31 @@ from http import HTTPStatus
 from django.test import TestCase
 from django.urls import reverse
 from django.test import Client
+from django.conf import settings
 
 from .tools import with_login
-from crossbox.models import Subscriber
 
 
 class AuthCase(TestCase):
 
-    def tearDown(self):
-        Subscriber.objects.all().delete()
-
-    def test_login(self):
-        self.assertIn('_auth_user_id', self.client.session)
-        self.client_no_auth = Client()
-        self.assertNotIn('_auth_user_id', self.client_no_auth.session)
+    fixtures = ['tests_base']
 
     @with_login()
     def test_auth_user_calls_auth_route_ok(self):
-        response = self.client.get(path=reverse('reservation'))
+        """ Middleware will be quite and let django resolve the url """
+        response = self.client.get(reverse('reservation'))
         self.assertEquals(response.status_code, HTTPStatus.OK)
 
     @with_login()
     def test_auth_user_calls_no_auth_route_redirect(self):
-        response = self.client.get(path=reverse('login'))
-        self.assertEquals(response.status_code, HTTPStatus.FOUND)
+        for no_auth_route in settings.NOT_LOGIN_REQUIRED_ROUTES:
+            path = (
+                reverse(no_auth_route)
+                if no_auth_route != 'password_reset_confirm'
+                else reverse(no_auth_route, args=['a', 'b'])
+            )
+            response = self.client.get(path)
+            self.assertEquals(response.status_code, HTTPStatus.FOUND)
 
     def test_no_auth_user_calls_auth_route_redirect(self):
         self.client_no_auth = Client()
