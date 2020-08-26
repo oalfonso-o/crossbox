14ab1cc7cf7d6f8ede63aea38cdd57460693e016
Oriol Alfonso Riba <oriolalfonso91@gmail.com>
2020-08-26 13:32:21 +0200

    Add admin mail to activate user

--- crossbox/views/user.py
+++ crossbox/views/user.py
@@ -14,18 +14,18 @@ from crossbox.views.stripe_webhooks import send_mail
 
 def get_mail_msg(user):
     message = MIMEMultipart('alternative')
-    message['Subject'] = f'Bienvenido a Crossbox {user.first_name}!'
+    message['Subject'] = f'Bienvenido a Crossbox {user.username}!'
     message['From'] = settings.SMTP_USER_NOTIFICATIONS
     message['To'] = user.email
     html = f'''\
 <html>
     <body>
-        <p>Hola {user.first_name} {user.last_name}</p>
+        <p>Hola {user.first_name} {user.last_name},</p>
         <br>
-        Gracias por registrarte en Crossbox, de momento tu usuario permanece
-        <b>desactivado</b> por lo que aún no podrás acceder a la aplicación
-        pero en breves nos pondremos en contacto contigo para confirmar el
-        alta.
+        Gracias por registrarte en Crossbox, todavía no hemos activado tu
+        usuario por lo que aún no podrás acceder a la aplicación
+        pero en breves lo activaremos. En cuanto lo hagamos recibirás un mail
+        de notifiación.
         <br>
         <br>
         <br>
@@ -51,6 +51,43 @@ def send_new_user_email(user):
     send_mail(msg, receivers)
 
 
+def get_mail_admin_msg(user):
+    message = MIMEMultipart('alternative')
+    message['Subject'] = f'Nuevo usuario {user.username}! Actívalo!'
+    message['From'] = settings.SMTP_USER_NOTIFICATIONS
+    message['To'] = user.email
+    html = f'''\
+<html>
+    <body>
+        <p>Hola admin, {user.first_name} {user.last_name} se ha registrado,</p>
+        <br>
+        Para activar este usuario:
+        <a href="{settings.BASE_URL}/admin/auth/user/{user.pk}/change/#/tab/module_2/">
+        Ir a su perfil</a>.
+        <br>
+        <br>
+        <br>
+        Atentamente,
+        <br>
+        El equipo de <a href="https://www.crossboxpalau.com/">Crossbox Palau
+        </a>
+    </p>
+  </body>
+</html>'''  # noqa
+    html_part = MIMEText(html, 'html')
+    message.attach(html_part)
+    return message.as_string()
+
+
+def send_new_user_email_admin(user):
+    msg = get_mail_admin_msg(user)
+    receivers = [
+        settings.SMTP_ADMIN_NOTIFICATIONS,
+        settings.SMTP_BOSS_NOTIFICATIONS,
+    ]
+    send_mail(msg, receivers)
+
+
 @require_http_methods(["GET", "POST"])
 def user_create(request):
     form = UserForm()
@@ -66,6 +103,7 @@ def user_create(request):
                 user.is_active = False  # user is activated manually by admin
                 user.save()
                 send_new_user_email(user)
+                send_new_user_email_admin(user)
                 return redirect('login')
             except ValidationError:
                 form.errors['password'] = (
